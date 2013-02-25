257687cd7329d33d12036e65ced54fc1257d0aa4
Camille Macalinao <cmacalin@gmail.com>
2013-02-25 01:33:04 -0800

    Started working on posecell_iteration and visual_template; lots more work still needed

--- ratSlam/src/main_ratslam.java
+++ ratSlam/src/main_ratslam.java
@@ -1,4 +1,5 @@
-
+// Divide methods to different files
+// Work on structs and other stuff
 
 public class main_ratslam 
 {
@@ -34,7 +35,7 @@ public class main_ratslam
 
 	// Specify the movie and the frames to read
 	int START_FRAME = 1;
-	int END_FRAME = 100; 		// Should be size of viddata
+	int END_FRAME = 100; 		// Should be size of viddata (photo, in our case)
 	int IMAGE_Y_SIZE = 100;		// Should be size of viddata
 	int IMAGE_X_SIZE = 100;		// Should be size of viddata
 
@@ -46,7 +47,7 @@ public class main_ratslam
 	double VT_GLOBAL_DECAY = 0.1;
 	double VT_ACTIVE_DECAY = 1.0;
 	double VT_SHIFT_MATCH = 20;
-	double MATCH_THRESHOLD = 0.09;
+	double VT_MATCH_THRESHOLD = 0.09;
 	int[] IMAGE_VT_Y_RANGE = setRange(IMAGE_Y_SIZE);
 	int[] IMAGE_VT_X_RANGE = setRange(IMAGE_X_SIZE);
 
@@ -78,6 +79,8 @@ public class main_ratslam
 	int POSECELL_VTRANS_SCALING = 1;
 	int ODO_FILE = 1;
 	
+	static int[][][] Posecells =  new int[PC_DIM_XY][PC_DIM_XY][PC_DIM_TH];
+	
 	// Main method of ratSLAM, not including constants (which are above, for the most part)
 	public static void main(String[] args) 
 	{
@@ -85,12 +88,13 @@ public class main_ratslam
 		int x_pc = (int) (Math.floor(PC_DIM_XY / 2.0) + 1);
 		int y_pc = (int) (Math.floor(PC_DIM_XY / 2.0) + 1);
 		int th_pc = (int) (Math.floor(PC_DIM_TH / 2.0) + 1);
-		int[][][] Posecells =  new int[PC_DIM_XY][PC_DIM_XY][PC_DIM_TH];
+		
 		Posecells[x_pc][y_pc][th_pc] = 1;
 		int[] max_act_xyth_path = {x_pc, y_pc, th_pc};
 
 		// Set the initial position in the odo and experience map
 		double[] odo = {0, 0, (PI / 2)};
+		
 		
 		// Process the parameters
 		
@@ -201,10 +205,180 @@ public class main_ratslam
 			image_x_sums[j] = image_x_sums[j] / avint;
 		} 
 		
-		// minoffset, mindiff styuff
+		// minoffset, mindiff stuff
 		int minoffset = 0;
 		vrot = minoffset *dpp * PI / 180;
 		prev_vrot_image_X_sums = image_x_sums;
+	}
+	
+	public void visual_template(int raw_image[][], int x, int y, int th)
+	{
+		int [][] sub_image = {IMAGE_VT_Y_RANGE,IMAGE_VT_X_RANGE};
+		int vt_id = 0;
+		// normalized intensity sums
+		int[] image_x_sums = {};
+		for (int i = 0; i < sub_image.length; i++)
+		{
+			for(int j = 0; j < sub_image[1].length; i++)
+			{
+				image_x_sums[i] = sub_image[i][j];
+			}
+		}
+		
+		int sumOf_image_x_sums = 0;
+		for (int i = 0; i < image_x_sums.length; i++)
+		{
+			sumOf_image_x_sums = image_x_sums[i];
+		}
+		
+		for (int j = 0; j < image_x_sums.length; j++)
+		{
+			image_x_sums[j]  =image_x_sums[j] / sumOf_image_x_sums;
+		}
+		
+		int[] minOffset = {};
+		int[] minDif = {};
+		
+		for (int i = 0; i < numvts; i++)
+		{
+			minOffset[i] = 1;
+			minDif[i] = 1;
+		}
+		
+		// get vt down before doing this loop
+		for (int k = 0; k < numvts; k++)
+		{
+			
+		}
+
+		// translate [diff, diff_id] = min(min_diff)
+		
+		//if this intensity template doesn't match any of the existing templates,
+		// then create a new template
+		// variable image_x_sums - where is it initialized
+		if (0 > VT_MATCH_THRESHOLD)
+		{
+			numvts++;
+			//vt(numvts).id = numvts;
+			// ...
+			vt_id = numvts;
+		}
+		else
+		{
+			// ...
+			if (prev_vt_id != vt_id)
+			{
+				// vt(vt_id).first = 0;
+			}
+		}
+		
+		// vt_history = [vt_history; vt_id] 
+	}
+	public void posecell_iteration(int vtId, double vTrans, double vRot)
+	{
+		// definitely need to construct vt
+		if (true)
+		{
+			// ...
+			int act_x = 0;
+			int act_y = 0;
+			int act_th = 0;
+			double energy = PC_VT_INJECT_ENERGY * (1/30) * (30 - Math.exp(1.2 * 1 /* replace 1 with vt(vt_id).template_decay */));
+			if (energy > 0)
+			{
+				Posecells[act_x][act_y][act_th] += energy;
+				
+			}
+		}
+		
+		// local excitation - PC_le = PC elements * PC weights
+		int[][][] pca_new = new int[PC_DIM_XY][PC_DIM_XY][PC_DIM_TH];
+		for (int i = 0; i < PC_DIM_XY; i++)
+		{
+			for (int j = 0; j < PC_DIM_XY; j++)
+			{
+				for (int k = 0; k < PC_DIM_TH; k++)
+				{
+					if (Posecells[i][j][k] != 0)
+					{
+						// do PC_E_XY_WRAP
+					}
+				}
+			}
+		}
+		
+		// local inhibition - PC_li = PC_le - PC_le elements * PC weights
+		pca_new = new int[PC_DIM_XY][PC_DIM_XY][PC_DIM_TH];
+		for (int x = 0; x < PC_DIM_XY; x++)
+		{
+			for (int y = 0; y < PC_DIM_XY; y++)
+			{
+				for (int z = 0; z < PC_DIM_TH; z++)
+				{
+					if (Posecells[x][y][z] != 0)
+					{
+						// do PC_I_XY_WRAP
+					}
+				}
+			}
+		}
+		
+		for (int a = 0; a < Posecells[0].length; a++)
+		{
+			for (int b = 0; b < Posecells[1].length; b++)
+			{
+				for (int c = 0; c < Posecells[2].length; c++)
+				{
+					Posecells[a][b][c] -= pca_new[a][b][c];
+				}
+			}
+		}
+		
+		// local global inhibition - PC_gi = PC_li elements - inhibition
+		// do this
+		
+		// Normalization
+		int total = 0;
+		for (int a = 0; a < Posecells[0].length; a++)
+		{
+			for (int b = 0; b < Posecells[1].length; b++)
+			{
+				for (int c = 0; c < Posecells[2].length; c++)
+				{
+					total += Posecells[a][b][c];
+				}
+			}
+		}
+		
+		for (int a = 0; a < Posecells[0].length; a++)
+		{
+			for (int b = 0; b < Posecells[1].length; b++)
+			{
+				for (int c = 0; c < Posecells[2].length; c++)
+				{
+					Posecells[a][b][c] = Posecells[a][b][c] / total;
+				}
+			}
+		}
+		
+		// PATH INTEGRATION
+		// vtrans affects xy direction
+		// shift in each th given b the th
+		for (int dir_pc = 0; dir_pc < PC_DIM_TH; dir_pc++)
+		{
+			double dir = (dir_pc -1) * PC_C_SIZE_TH;
+			if (dir == 0)
+			{
+				for (int x = 0; x < Posecells[0].length; x++)
+				{
+					for (int y = 0; y < Posecells[1].length; y++)
+					{
+						// Posecells[x][y][dir_pc] =  
+					}
+				}
+			}
+		}
+		
 	}
 	
 	// Creates 1-dimensional arrays that mimic MATLAB colon operator
